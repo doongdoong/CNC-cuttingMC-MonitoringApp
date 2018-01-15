d7a07dcd44c18f5a77b00f05f6a26bf6f51d56e8
jinwhajung <jinhwajung93@gmail.com>
2018-01-15 21:16:46 +0900

    noti change

--- app/src/main/java/kr/ac/kmu/ncs/cnc_mc_monitor/FCM/MyFirebaseMessagingService.java
+++ app/src/main/java/kr/ac/kmu/ncs/cnc_mc_monitor/FCM/MyFirebaseMessagingService.java
@@ -40,6 +40,13 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
             Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
             sendNotification(remoteMessage.getNotification());
         }
+        if(remoteMessage.getData().size() > 0){
+            Log.d(TAG, "Message data payload : " + remoteMessage.getData());
+            sendData(remoteMessage);
+        }else{
+            Log.d(TAG, "Can't Receive Message data");
+        }
+
     }
     // [END receive_message]
 
@@ -81,4 +88,26 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
     }
+
+    private void sendData(RemoteMessage remoteMessage) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(remoteMessage.getData().get("title"))
+                .setContentText(remoteMessage.getData().get("body"))
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        int id = (int)remoteMessage.getData().get("title").toString().charAt(1);
+        notificationManager.notify(id/* ID of notification */, notificationBuilder.build());
+    }
+
 }
